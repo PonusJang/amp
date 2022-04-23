e425e1e93b183c1593b731315c11b2ea65153d9f
PonusJang <ponusjang@gmail.com>
2022-04-23 16:14:37 +0800

    update

--- apiv1/dns.go
+++ apiv1/dns.go
@@ -4,9 +4,9 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
-type HelloReq struct {
+type DnsAddReq struct {
 	g.Meta `path:"/hello" tags:"Hello" method:"get" summary:"You first hello api"`
 }
-type HelloRes struct {
+type DnsAddRes struct {
 	g.Meta `mime:"text/html" example:"string"`
 }
